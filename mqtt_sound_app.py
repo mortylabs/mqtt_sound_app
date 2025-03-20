424ff363ad0fceb8dabaf1cf1bb99cbef67fb15b
Helga UK <mortyhomeuk@gmail.com>
2025-03-20 18:02:58 +0000

    make telegram message sending optional

--- mqtt_sound_app.py
+++ mqtt_sound_app.py
@@ -41,6 +41,10 @@ if MQTT_USER and MQTT_PASS:
 
 def send_telegram_message(message):
     """ Send a message to Telegram """
+    if not TELEGRAM_BOT_TOKEN or not TELEGRAM_CHAT_ID:
+        logging.debug("Telegram notifications are disabled. Skipping message.")
+        return  # Exit without sending anything
+
     if TELEGRAM_BOT_TOKEN and TELEGRAM_CHAT_ID:
         telegram_url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
         payload = {"chat_id": TELEGRAM_CHAT_ID, "text": message}
